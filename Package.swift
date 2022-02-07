91b77e32fb2ffc37cc1bcc73cf86743a8e53c9bb
shem <vsv@scade.io>
2022-02-07 13:26:38 +0200

    add linking with ScadeKit

--- Package.swift
+++ Package.swift
@@ -1,6 +1,9 @@
 // swift-tools-version:5.3
 
 import PackageDescription
+import Foundation
+
+let SCADE_SDK = ProcessInfo.processInfo.environment["SCADE_SDK"] ?? ""
 
 let package = Package(
     name: "FusionCamera",
@@ -29,7 +32,11 @@ let package = Package(
             name: "FusionCamera_Apple",
             dependencies: [
               .target(name: "FusionCamera_Common"),
-            ]                        
+            ],
+            swiftSettings: [
+                .unsafeFlags(["-F", SCADE_SDK], .when(platforms: [.macOS, .iOS])),
+                .unsafeFlags(["-I", "\(SCADE_SDK)/include"], .when(platforms: [.android])),
+            ]
         ),            	
         // .target(
         //     name: "FusionCamera_Android",
